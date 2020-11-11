31d54c790aecb2c992407e3812bbdeaceac488f1
ssMMiles <admin@milesmoonlove.com>
2020-11-11 21:58:00 +0100

    replaced canvas gradient with css

--- components/Background.js
+++ components/Background.js
@@ -1,35 +1,8 @@
-import React, { useRef, useEffect } from 'react';
-
-import Gradient from './Gradient.js';
 import styles from '../styles/components/Background.module.css'
 
 export default function Background() {
-    const ref = useRef();
-
-    useEffect(() => {
-
-        const gradient = new Gradient(ref.current)
-
-        let requestId;
-
-        function render() {
-            gradient.update();
-
-            requestId = requestAnimationFrame(render);
-        }
-
-        render();
-
-        return () => {
-            cancelAnimationFrame(requestId)
-        }
-    });
 
     return (
-        <div>
-            <canvas className={styles.bg}
-                ref={ref}
-            />
-        </div>
+        <div className={styles.bg} />
     );
 }
